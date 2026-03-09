6b28a33b1506da97339dd665ef5325474bf2811d
Francisco Castel <146264604+fcastel2002@users.noreply.github.com>
2026-03-09 07:51:51 -0300

    First

---
+++ ejercicio_1/main.c
@@ -0,0 +1,20 @@
+#include <math.h>
+#include <stdio.h>
+
+
+void main(void)
+{
+    signed char a, b, c, d, s1, s2;
+    a = 127;
+    b = 127;
+    c = a + b;
+    d = a * b;
+    s1 = (-8) >> 2;
+    s2 = (-1) >> 5;
+
+    printf("c = %d \n", c);
+    printf("d = %d \n", d);
+    printf("s1 = %d \n", s1);
+    printf("s2 = %d \n", s2);
+    
+}
